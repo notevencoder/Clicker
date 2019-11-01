1468d9b04436e6c8f04d9fcf55b8a6752412a0bf
Resst <55293545+Resst@users.noreply.github.com>
2019-11-01 21:36:34 +0300

    Create

--- core/src/com/mygdx/game/Clicker.java
+++ core/src/com/mygdx/game/Clicker.java
@@ -6,26 +6,22 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class Clicker extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	Field fl;
-
+	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		fl = new Field();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-
-
-		//batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
